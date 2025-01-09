5a4eb9ad81d5352d148c35f1f2a7d2f650862970
Areeb Durrani <areeb702@revature.net>
2025-01-09 13:18:51 -0800

    removed unused file

---
+++ funZone/Controller/CustomTableCells.swift
@@ -0,0 +1,24 @@
+//
+//  TableCellBook.swift
+//  funZone
+//
+//  Created by admin on 12/31/24.
+//
+
+import UIKit
+
+class UITableViewCellBook: UITableViewCell {
+    @IBOutlet weak var titleBook: UILabel!
+    @IBOutlet weak var iconBook: UIImageView!
+}
+
+class UITableViewCellMusic: UITableViewCell {
+    @IBOutlet weak var titleMusic: UILabel!
+    @IBOutlet weak var iconMusic: UIImageView!
+}
+
+class UICustomTableCell: UITableViewCell {
+    @IBOutlet weak var cellTitle: UILabel!
+    @IBOutlet weak var cellIcon: UIImageView!
+}
+
